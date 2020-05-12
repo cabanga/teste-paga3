b41d3a3d15e67cd09afd78c87e0cd946055767b0
Fernando dos Santos <nando4803@gmail.com>
2020-05-12 21:18:20 +0100

    add dashboard-profile welcome

--- src/components/Web/DashboardWelcome/index.js
+++ src/components/Web/DashboardWelcome/index.js
@@ -1,39 +1,34 @@
 import React from 'react';
 import {
-  FaTimes,
   FaShoppingBasket,
-  FaChevronDown
+  FaChevronDown,
+  FaUserAlt
 } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
-import Woman from '../../../assets/woman.jpg';
+import Ballons from '../../../assets/ballons.svg';
 
 function Dashboard() {
   return (
-    <div className="dash-container">
-      <div className="dash-content">
-        <aside className="dashflex dashgrid">
-          <div className="shopping">
-            <FaShoppingBasket className="shopping-icon" />
+    <div className="dashboardContainer">
+        <aside className="dashboardFlex">
+          <section className="dashboardShopping">
+            <FaShoppingBasket className="shoppingIcon" />
             <h1>Fazer compra</h1>
-          </div>
-          <div className="notification">
+          </section>
+          <section className="dashboardNotification">
             <h3>Notificações</h3>
-            <FaChevronDown className="chevron" />
-          </div>
-          <div className="notification-card">
+            <FaChevronDown className="notificationChevron" />
+          </section>
+          <section className="notificationCard">
             <span>12 Ago 2020</span>
             <p>Nova compra efetuada no valor de 30.000 kz</p>
-          </div>
-          <div className="notification-card">
-            <span>12 Ago 2020</span>
-            <p>Nova compra efetuada no valor de 30.000 kz</p>
-          </div>
+          </section>
       </aside>
 
-        <main className="dashflex dashgrid">
-          <div className="dash">
+        <main className="dashboardFlex">
+          <section className="dashboard">
             <h1>Dashboard de Gestão</h1>
             <h3>Gerencie melhor as suas contas</h3>
             <ul>
@@ -41,64 +36,28 @@ function Dashboard() {
               <li><Link to="/">Em pagamento</Link></li>
               <li><Link to="/">Pagas</Link></li>
             </ul>
-          </div>
-    
-          <div className="dash-card">
-            <span>Nome do produto</span>
-            <p>Computador HP i7 8654</p>
-            <span>Loja</span>
-            <p>NCR Angola</p>
-            <span>Data de aquisição</span>
-            <p>12 Ago 2020</p>
-          </div>
-          <div className="dash-card">
-            <span>Nome do produto</span>
-            <p>Computador HP i7 8654</p>
-            <span>Loja</span>
-            <p>NCR Angola</p>
-            <span>Data de aquisição</span>
-            <p>12 Ago 2020</p>
-          </div>
-      </main>  
-      
-        <aside className="dashflex dashgray dashgrid">
-          <div className="dash-profile">
-            <p>Debora Leandro</p>
-            <img src={Woman} alt="Perfil" />
-            <span className="time-icon"><FaTimes /></span>  
-          </div>
-      
-        <h2>Debora Emanuel Leandro</h2>
-        <h4>Conta Verificada</h4>
-        <div className="dash-info">
-            <div className="dash-info-card">
-              <h3>Salário</h3>
-              <p>150.2 m</p>
-            </div>
-            <div className="dash-info-card">
-              <h3>Salário</h3>
-              <p>150.2 m</p>
-            </div>
-        </div>
-      
-          <div className="dash-info-bank">
-            <span>Nome do Banco:</span>
-            <p>Banco Millennium Atlantico</p>
-            <span>IBAN:</span>
-            <p>AO6 0000000000000022</p>
-            <span>Nome do Banco:</span>
-            <p>Banco Millennium Atlantico</p>
-            <span>IBAN:</span>
-            <p>AO6 0000000000000022</p>  
-          </div>
+          </section>
+          <section className="dashboardWelcome">
+            <img src={Ballons} alt=""/>
+            <h1>Seja bem-vindo</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione magnam in, praesentium asperiores neque sequi tempore animi cum incidunt nemo, vitae repudiandae minima eveniet at non cupiditate, inventore corporis delectus!
+            </p>
+          </section>
+      </main>
 
-        <section className="btn-out">
-          <Link className="btn-red" to="/">Sair</Link>
-        </section>  
+      <aside className="dashboardProfile">
+        <section className="dashboardProfileInfo">
+          <p>Debora Leandro</p>
+          <div className="userIcon">
+            <FaUserAlt className="userAlt" />
+          </div>
+        </section>
+        <section className="btn-home">
+          <Link className="btn-sair" to="/">Sair</Link>
+        </section> 
       </aside>
-    
-      </div>   
-    </div>
+    </div> 
   )
 }
 
