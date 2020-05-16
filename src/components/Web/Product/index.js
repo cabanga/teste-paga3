c1b838df7c279e01524addd2ebd1837f40ce1145
Fernando dos Santos <nando4803@gmail.com>
2020-05-16 21:12:00 +0100

    add dinamic content

--- src/components/Web/Product/index.js
+++ src/components/Web/Product/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Woman from '../../../assets/woman.jpg';
 import './styles.css';
 
+import api from '../../../services/api';
+
 function Product() {
+  const [productName, setProductName] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+  const [productCode, setProductCode] = useState('');
+  const [productImage, setProductImage] = useState('');
+  const [productDescription, setProductDescription] = useState('');
+
   const sellerName = localStorage.getItem('sellerName');
   const sellerIban = localStorage.getItem('sellerIban');
   const sellerAddress = localStorage.getItem('sellerAddress');
+
+  async function handleSubmitProduct(e) {
+    e.preventDefault();
+
+    setProductName('');
+    setProductPrice('');
+    setProductPrice('');
+    setProductCode('');
+    setProductImage('');
+    setProductDescription('');
+
+    const data = {
+      productName,
+      productPrice,
+      productCode,
+      productImage,
+      productDescription
+    };
+
+    try {
+      const response = await api.post('products', data)
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className="product">
       <aside className="product-profile">
@@ -25,17 +59,55 @@ function Product() {
           <h3>Gerencie aqui os produtos da loja</h3>
         </div>
         <div className="main-form">
-          <form>
+          <form onSubmit={handleSubmitProduct}>
             <label>Nome do produto</label>
-            <input type="text" placeholder="Computador HP i7 8654" />
-            <label>Código do produto</label>
-            <input type="text" placeholder="Código de refêrencia" />
+            <input
+              type="text"
+              name="productName"
+              value={productName}
+              onChange={e => setProductName(e.target.value)}
+              placeholder="Computador HP i7 8654"
+            />
             <label>Preço</label>
-            <input type="text" placeholder="A0A 250.000,00"/>
+            <input
+              type="text"
+              name="productPrice"
+              value={productPrice}
+              onChange={e => setProductPrice(e.target.value)}
+              placeholder="A0A 250.000,00"
+            />
+            <label>Código do produto</label>
+            <input
+              type="text"
+              name="productCode"
+              value={productCode}
+              onChange={e => setProductCode(e.target.value)}
+              placeholder="Código de refêrencia"
+            />
+            <label>Imagem do produto</label>
+            <input
+              type="file"
+              name="productImage"
+              value={productImage}
+              onChange={e => setProductImage(e.target.value)}
+              id="image"
+            />
+            <label>Descrição do produto</label>
+            <textarea
+              name="productDescription"
+              value={productDescription}
+              onChange={e => setProductDescription(e.target.value)}
+              id="description"
+              cols="30"
+              rows="10"
+              placeholder="Breve discrição do produto"
+            ></textarea>
+            <div className="btn-product">
+              <button type="submit">
+                Adicionar novo produto
+              </button>
+            </div>
           </form>
-        </div>
-        <div className="btn-product">
-          <input type="button" value="Adicionar novo produto" />
         </div>
       </main>
     </div>
