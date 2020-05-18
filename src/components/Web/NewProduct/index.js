c9806c3a846951e411261c75b15e2127e0b21f9f
Fernando dos Santos <nando4803@gmail.com>
2020-05-18 20:55:11 +0100

    fix global style and add new product page

--- src/components/Web/NewProduct/index.js
+++ src/components/Web/NewProduct/index.js
@@ -1,92 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
-import api from '../../../services/api';
+import './styles.css';
 
-function ProductNew() {
-  const [productName, setProductName] = useState('');
-  const [productPrice, setProductPrice] = useState('');
-  const [productCode, setProductCode] = useState('');
-  const [productImage, setProductImage] = useState('');
-  const [productDescription, setProductDescription] = useState('');
-
-  async function handleSubmitProduct(e) {
-    e.preventDefault();
-
-    setProductName('');
-    setProductPrice('');
-    setProductPrice('');
-    setProductCode('');
-    setProductImage('');
-    setProductDescription('');
-
-    const data = {
-      productName,
-      productPrice,
-      productCode,
-      productImage,
-      productDescription
-    };
-
-    try {
-      const response = await api.post('products', data)
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-  }
+function NewProduct() {
+  
   return (
-    <div className="main-form">
-          <form onSubmit={handleSubmitProduct}>
-            <label>Nome do produto</label>
-            <input
-              type="text"
-              name="productName"
-              value={productName}
-              onChange={e => setProductName(e.target.value)}
-              placeholder="Computador HP i7 8654"
-            />
-            <label>Preço</label>
-            <input
-              type="text"
-              name="productPrice"
-              value={productPrice}
-              onChange={e => setProductPrice(e.target.value)}
-              placeholder="A0A 250.000,00"
-            />
-            <label>Código do produto</label>
-            <input
-              type="text"
-              name="productCode"
-              value={productCode}
-              onChange={e => setProductCode(e.target.value)}
-              placeholder="Código de refêrencia"
-            />
-            <label>Imagem do produto</label>
-            <input
-              type="file"
-              name="productImage"
-              value={productImage}
-              onChange={e => setProductImage(e.target.value)}
-              id="image"
-            />
-            <label>Descrição do produto</label>
-            <textarea
-              name="productDescription"
-              value={productDescription}
-              onChange={e => setProductDescription(e.target.value)}
-              id="description"
-              cols="30"
-              rows="10"
-              placeholder="Breve discrição do produto"
-            ></textarea>
-            <div className="btn-product">
-              <button type="submit">
-                Adicionar novo produto
-              </button>
-            </div>
-          </form>
-        </div>
+    <div className="new-product">
+      <div className="content">
+        <section>
+        <h1>Paga3</h1>
+        
+        <h1>Cadastrar novo produto</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+          
+          <Link to="/profile" className="back-link">
+            <FiArrowLeft size={16} color="#E02041"/>
+           Voltar no Perfil
+          </Link>
+        </section>
+        <form>
+          <input
+            type="text"  
+            placeholder="Nome do produto" 
+          />
+          <textarea 
+            placeholder="Descrição do produto"
+          />
+          <input
+            type="text"  
+            placeholder="Valor em kwanza"
+          />
+        </form>
+      </div>
+    </div>
   )
 }
 
-export default ProductNew;
+export default NewProduct;
