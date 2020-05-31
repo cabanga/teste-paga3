e868e957856c4f7040d998c41a8ec6dd17512b24
Fernando dos Santos <nando4803@gmail.com>
2020-05-31 20:25:07 +0100

    This commit add store-edit page

--- src/pages/EditStore/index.js
+++ src/pages/EditStore/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
 function EditStore() {
   return (
-    <h1>Edit Store</h1>
+    <div className="store-container">
+        <section className="store-form">
+          <Link to="/user/dashboard" className="back-link">
+            <FiArrowLeft size={20} color="#F94545"/>
+            Voltar no Perfil
+          </Link>
+         <h1>Editar as informações da Loja</h1>
+          <form>
+            <input type="text" placeholder="Nome da Loja" required />
+            <input type="text" placeholder="Telefone da Loja" required   />
+            <input type="text" placeholder="Conta bancaria da Loja" required />
+            <input type="tel" placeholder="IBAN da Loja" required />
+        </form>
+        <button className="button" type="submit">Salvar</button>
+        </section>
+      </div>
   )
 }
 
