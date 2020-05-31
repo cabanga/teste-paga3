37237fd0d74898bd5032d5f37355822588db85e2
Fernando dos Santos <nando4803@gmail.com>
2020-05-31 20:26:05 +0100

    This commit fix broken link

--- src/pages/StoreProfile/index.js
+++ src/pages/StoreProfile/index.js
@@ -1,5 +1,5 @@
 import React  from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaTrash } from 'react-icons/fa';
 
 import Woman from '../../assets/woman.jpg';
