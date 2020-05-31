3aa88a12180a9fea0c442852dd85fa0ebef05ede
Fernando dos Santos <nando4803@gmail.com>
2020-05-31 18:47:47 +0100

    This commit add edit profile page

---
+++ src/pages/SellerEditProfile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+function SellerEditProfile() {
+  return (
+    <h1>Oi</h1>
+  )
+}
+
+export default SellerEditProfile;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import FindProduct from './pages/FindProduct/';
 
 import UserEditProfile from './pages/UserEditProfile/';
+import SellerEditProfile from './pages/SellerEditProfile/';
 import UserProfile from './pages/UserProfile/';
 import SellerProfile from './pages/SellerProfile/';
 import NewStore from './pages/NewStore/';
@@ -25,6 +26,7 @@ export default props => (
         
         <Route exact path="/comprar" component={FindProduct} />
         <Route exact path="/edit-info-user" component={UserEditProfile} />
+        <Route exact path="/edit-info-seller" component={SellerEditProfile} />
         <Route exact path="/user/dashboard" component={UserProfile} />
         <Route exact path="/seller/dashboard" component={SellerProfile} />
         <Route exact path="/seller/new-store" component={NewStore} />
