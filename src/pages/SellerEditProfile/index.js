92f30a011649a80b3aac80d5af550cbe3c38656e
Fernando dos Santos <nando4803@gmail.com>
2020-05-31 18:57:08 +0100

    This commit add new edit profile page

--- src/pages/SellerEditProfile/index.js
+++ src/pages/SellerEditProfile/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
 function SellerEditProfile() {
   return (
-    <h1>Oi</h1>
+    <div className="edite-container">
+        <section className="form-edite">
+          <Link to="/user/dashboard" className="back-link">
+            <FiArrowLeft size={20} color="#F94545"/>
+            Voltar no Perfil
+          </Link>
+         <h1>Editar as informações da Loja</h1>
+          <form>
+            <input type="text" placeholder="Nome da Loja" required />
+            <input type="text" placeholder="Telefone da Loja" required   />
+            <input type="text" placeholder="Conta bancaria da Loja" required />
+            <input type="tel" placeholder="IBAN da Loja" required />
+        </form>
+        <button className="button" type="submit">Salvar</button>
+        </section>
+      </div>
   )
 }
 
