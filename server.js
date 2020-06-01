31e30c516e68b4e249a8a49b93709cd95c6b5ff0
cabanga <manucabanga@gmail.com>
2020-06-01 11:10:10 +0100

    config server

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
-
-const app = express()
-
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
-
-app.use(staticFileMiddleware)
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}))
-app.use(staticFileMiddleware)
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'))
-})
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port
-  console.log('App now running on port', port)
-})
+//server.js
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
