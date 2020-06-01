4c9c4e585598acf539dac9f103bf5691d30cbcb2
cabanga <manucabanga@gmail.com>
2020-06-01 09:48:19 +0100

    test deploy in heroku

---
+++ build/precache-manifest.eea12fb36e1ac3c963bb4e59ca47792e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cc1329c5f2149caf8a38eb4bb77e80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76be241b54937a77c797",
+    "url": "/static/css/main.3724df5f.chunk.css"
+  },
+  {
+    "revision": "ce19cd044f970cc9bcb6",
+    "url": "/static/js/2.fe3d46fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fe3d46fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76be241b54937a77c797",
+    "url": "/static/js/main.8ab42563.chunk.js"
+  },
+  {
+    "revision": "1edd5cb14e0ed6a4bcd2",
+    "url": "/static/js/runtime-main.5a5f3741.js"
+  },
+  {
+    "revision": "2ce93c65ec24e2abc7b1d58e0b2cbd04",
+    "url": "/static/media/analitics.2ce93c65.svg"
+  },
+  {
+    "revision": "cffb3b736f76860e859e28e41af7b74c",
+    "url": "/static/media/background.cffb3b73.png"
+  },
+  {
+    "revision": "942f9d2e3cc30d8ed4b4377be438f0fc",
+    "url": "/static/media/fall.942f9d2e.svg"
+  },
+  {
+    "revision": "bd0ad6b8ec65afdd6b26fb92fa1a4ef4",
+    "url": "/static/media/woman.bd0ad6b8.jpg"
+  }
+]);
